098d9575114b1b3d892b634097762d30f0b87530
0Accelerator0 <948386361@qq.com>
2018-11-30 10:18:49 +0800

    Added structure to receive information

--- myAI.js
+++ myAI.js
@@ -1,11 +1,13 @@
 
 const io = require('socket.io-client');
-//var socket = io('https://namabilly-gomoku.herokuapp.com');
-var socket = io('http://localhost:3000');
+var socket = io('https://namabilly-gomoku.herokuapp.com');
+//var socket = io('http://localhost:3000');
 
+var SIZE = {x: 15, y: 15};
+var g;
 var gid = -1;
 var name = 'myAI';
-var turn = 0;
+var side = 0;
 
 socket.on('connect', () => {
 	console.log(socket.connected);
@@ -32,6 +34,7 @@ socket.on('joinGameResponse', (data) => {
 	if (data.success){
 		gid = data.id;
 		console.log('Joined game 10' + gid + '.');
+		g = new Game(SIZE);
 	}
 	else {
 		console.log(data.msg);
@@ -40,19 +43,42 @@ socket.on('joinGameResponse', (data) => {
 
 socket.on('updateBoard', (data) => {
 	console.log('received.');
-	turn = data.pieces.length;
-	if (turn%2 == 1) {
+	g.load(data.pieces);
+	console.log(g.matrix);
+	if (!data.lock) {
 		socket.emit('put', {
 			id: gid,
 			position: {
 				x: Math.floor(Math.random()*15),
 				y: Math.floor(Math.random()*15)
 			},
-			name: name,
-			turn: turn
+			name: name
 		});
 	}
 });
+
+class Game {
+	constructor(size){
+		this.size = size;
+		this.matrix = [];
+		this.init();
+	}
+	init(){
+		for (let x=0;x<this.size.x;x++) {
+			this.matrix[x] = [];
+			for (let y=0;y<this.size.y;y++) {
+				this.matrix[x][y] = 0;
+			}
+		}
+	}
+	load(matrix){
+		this.init();
+		for (let i in matrix) {
+			var piece = matrix[i];
+			this.matrix[piece.position.x][piece.position.y] = piece.turn%2*2-1;
+		}
+	}
+}
 
 
 
